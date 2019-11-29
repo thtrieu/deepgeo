0c48ba03b7a881dcf5ec70a32fb7c6fac8dac0c7
thtrieu <thtrieu@apcs.vn>
2019-11-29 15:21:08 -0500

    Create t2t_eval.py

---
+++ t2t_eval.py
@@ -0,0 +1,13 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensor2tensor.bin import t2t_eval
+
+import model
+import problem
+
+
+if __name__ == '__main__':
+  t2t_eval.main(None)
